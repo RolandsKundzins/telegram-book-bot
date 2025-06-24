a3f201fdc8d2b24729e47e904b519a2af33bfdfc
Rolands Kundzins <rolis.kungs@inbox.lv>
2025-06-24 11:12:53 +0300

    Send email to pocketbook - in progress

---
+++ supabase/functions/telegram-book-bot/emailUtils.ts
@@ -0,0 +1,42 @@
+
+const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY')
+
+
+function encodeBase64(data: Uint8Array): string {
+  const chunkSize = 0x8000; // 32KB chunks
+  let binary = '';
+  for (let i = 0; i < data.length; i += chunkSize) {
+    const chunk = data.subarray(i, i + chunkSize);
+    binary += String.fromCharCode(...chunk);
+  }
+  return btoa(binary);
+}
+
+export async function sendEmail(to: string, subject: string, fileName: string, fileData: Uint8Array): Promise<void> {
+  const res = await fetch('https://api.resend.com/emails', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      from: 'onboarding@resend.dev', // TODO: Need to use my actual email address here
+      to,
+      subject,
+      html: 'Book that is uploaded to Pocketbook using "send to Pocketbook" feature',
+      attachments: [
+      {
+        filename: fileName,
+        content: encodeBase64(fileData),
+      },
+    ],
+    }),
+  })
+  const data = await res.json()
+  if (!res.ok) {
+    console.error('Failed to send email:', data);
+    throw new Error('Email send failed');
+  } else {
+    console.log('Email sent successfully:', data);
+  }
+}
--- supabase/functions/telegram-book-bot/index.ts
+++ supabase/functions/telegram-book-bot/index.ts
@@ -1,6 +1,7 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { sendTelegramMessage, sendTelegramDocument } from "./telegramUtils.ts";
 import { searchAnnasArchive, formatBooksMarkdown, getLibgenDownloadUrl, downloadBookFile } from "./bookLibraryUtils.ts";
+import { sendEmail } from "./emailUtils.ts";
 
 Deno.serve(async (req) => {
   const body = await req.json();
@@ -28,7 +29,7 @@ Deno.serve(async (req) => {
 async function handleDownloadRequest(chatId: number, message: string) {
   console.log("handleDownloadRequest");
 
-  await sendTelegramMessage(chatId, `Download request received`);
+  await sendTelegramMessage(chatId, `Download request received. \nLooking for download link...`);
   const bookId = message.slice(3).trim();
   const downloadLink = await getLibgenDownloadUrl(bookId);
 
@@ -38,14 +39,22 @@ async function handleDownloadRequest(chatId: number, message: string) {
     return;
   }
 
-  await sendTelegramMessage(chatId, `Downloading book...\nThis may take a minute`); // TODO: maybe add progress msges
+  await sendTelegramMessage(chatId, `Downloading book...\nThis may take a few minutes`); // TODO: maybe add progress msges
 
   const filename = `${bookId}.epub`;
   const bookData = await downloadBookFile(downloadLink);
 
   await sendTelegramMessage(chatId, `Book downloaded. Uploading to Telegram...`);
   await sendTelegramDocument(chatId, filename, bookData);
-  await sendTelegramMessage(chatId, `Enjoy your book! 📚`);
+  // await sendTelegramMessage(chatId, `Enjoy your book! 📚`);
+
+  await sendTelegramMessage(chatId, `Uploading book to Pocketbook via "send to Pocketbook" feature... 📤`);
+  try {
+    await sendEmail("rolands.kungs@pbsync.com", "Title of email", filename, bookData);  // TODO: Need to use my actual email address here
+    await sendTelegramMessage(chatId, `Book successfully sent to Pocketbook! ✉️\nTap "Sync" on your Pocketbook to access it.`);
+  } catch (_error) {
+    await sendTelegramMessage(chatId, "⚠️ Uh-oh! It looks like the bot can't send the book to Pocketbook. \nPlease check your email and allow the bot to send to Pocketbook. ⚠️");
+  }
 }
 
 
@@ -55,6 +64,11 @@ async function handleSearchRequest(chatId: number, message: string) {
   await sendTelegramMessage(chatId, "Will give you a few book options in a moment...");
 
   const bookList = await searchAnnasArchive(message);
+
+  if(bookList.books.length === 0) {
+    await sendTelegramMessage(chatId, "No books found for your search query. Please try again with a different query.");
+    return;
+  }
 
   for (const book of bookList.books) {
     const bookInfoMarkdown = formatBooksMarkdown(book);
